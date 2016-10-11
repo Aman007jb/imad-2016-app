6de50e07de104932aa4ce11d6e0703912a40f4ea
Aman007jb <amansk362@gmail.com>
2016-10-11 19:44:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one is here');
+  res.send('Article one is here');
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile('Article two is here');
+  res.send('Article two is here');
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile('Article three is here');
+  res.send('Article three is here');
 });
 
 app.get('/ui/style.css', function (req, res) {
