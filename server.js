405fbee2881cbb31da08863f53ba4294a9e92590
Aman007jb <amansk362@gmail.com>
2016-10-22 15:20:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article', function (req, res) {
+app.get('/article.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 
